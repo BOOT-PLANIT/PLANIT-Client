070b1ddd65021b56dc62fcc93c3d65cb33d86ad5
leehoegyu <leehoegyu@gmail.com>
2025-12-04 17:01:08 +0900

    fix: 로고 사이즈 수정

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -20,7 +20,7 @@ const Header = ({ title, userName, children }: HeaderProps) => {
       <div className={styles.container}>
         <Link href="/" className={styles.logo}>
           <div className={styles.logoIcon}>
-            <Image alt="logo" src="logo.svg" width="40" height="40" />
+            <Image alt="logo" src="logo.svg" width={40} height={40} />
           </div>
           <span className={styles.logoText}>{title}</span>
         </Link>
