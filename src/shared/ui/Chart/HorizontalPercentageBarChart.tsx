04e7373a08053e35bfbd950a120c6722acf1b3bf
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-12-11 10:14:58 +0900

    fix: 애니메이션 수정

--- src/shared/ui/Chart/HorizontalPercentageBarChart.tsx
+++ src/shared/ui/Chart/HorizontalPercentageBarChart.tsx
@@ -27,8 +27,8 @@ function createSingleBarData(item: BarChartItem) {
   return [
     {
       name: SINGLE_BAR_KEY,
+      [item.label]: percentage, // 실제 데이터 Bar
       [BACKGROUND_LABEL]: 100, // 배경 Bar
-      [item.label]: percentage,
     },
   ];
 }
@@ -42,33 +42,39 @@ const BarChartBase = ({ data, height = 20 }: BarChartProps) => {
   const stackedData = createSingleBarData(data);
 
   return (
-    <ResponsiveContainer width="100%" height={height}>
-      <ReBarChart
-        layout="vertical"
-        data={stackedData}
-        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
-      >
-        <XAxis type="number" domain={[0, 100]} hide reversed={true} />
-        <YAxis type="category" dataKey="name" hide />
+    <div
+      style={{
+        width: "100%",
+        pointerEvents: "none",
+      }}
+    >
+      <ResponsiveContainer width="100%" height={height}>
+        <ReBarChart
+          layout="vertical" // x축 방향
+          data={stackedData}
+          margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+        >
+          <XAxis type="number" domain={[0, 100]} hide />
+          <YAxis type="category" dataKey="name" hide />
 
-        {/* 배경 Bar*/}
-        <Bar
-          key={BACKGROUND_LABEL}
-          dataKey={BACKGROUND_LABEL}
-          fill={BACKGROUND_COLOR}
-          isAnimationActive={false}
-          stackId="a" // 실제 bar와 겹치게 하기 위한 요소
-        />
+          {/* 실제 데이터 Bar */}
+          <Bar
+            key={data.label}
+            dataKey={data.label}
+            fill={data.color}
+            stackId="a"
+          />
 
-        {/* 실제 데이터 Bar */}
-        <Bar
-          key={data.label}
-          dataKey={data.label}
-          fill={data.color}
-          stackId="a"
-        />
-      </ReBarChart>
-    </ResponsiveContainer>
+          {/* 배경 Bar */}
+          <Bar
+            key={BACKGROUND_LABEL}
+            dataKey={BACKGROUND_LABEL}
+            fill={BACKGROUND_COLOR}
+            stackId="a"
+          />
+        </ReBarChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
