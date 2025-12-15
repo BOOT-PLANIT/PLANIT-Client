1a96b9bbbaf3fb96ca7ea2d4a1bfd2da530aaf0f
leehoegyu <leehoegyu@gmail.com>
2025-12-15 16:44:33 +0900

    fix: font path

--- src/shared/assets/font/font.ts
+++ src/shared/assets/font/font.ts
@@ -3,23 +3,23 @@ import localFont from "next/font/local";
 export const Pretendard = localFont({
   src: [
     {
-      path: "../shared/assets/font/pretendard/woff2/Pretendard-Light.subset.woff2",
+      path: "./pretendard/woff2/Pretendard-Light.subset.woff2",
       weight: "300",
     },
     {
-      path: "../shared/assets/font/pretendard/woff2/Pretendard-Regular.subset.woff2",
+      path: "./pretendard/woff2/Pretendard-Regular.subset.woff2",
       weight: "400",
     },
     {
-      path: "../shared/assets/font/pretendard/woff2/Pretendard-Medium.subset.woff2",
+      path: "./pretendard/woff2/Pretendard-Medium.subset.woff2",
       weight: "500",
     },
     {
-      path: "../shared/assets/font/pretendard/woff2/Pretendard-SemiBold.subset.woff2",
+      path: "./pretendard/woff2/Pretendard-SemiBold.subset.woff2",
       weight: "600",
     },
     {
-      path: "../shared/assets/font/pretendard/woff2/Pretendard-Bold.subset.woff2",
+      path: "./pretendard/woff2/Pretendard-Bold.subset.woff2",
       weight: "700",
     },
   ],
