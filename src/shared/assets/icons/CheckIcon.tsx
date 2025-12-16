eb0ee3e0a7f5938dd49625b046f86a93b220fe16
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-12-16 15:29:52 +0900

    refactor: 로그인 페이지 리팩토링

---
+++ src/shared/assets/icons/CheckIcon.tsx
@@ -0,0 +1,20 @@
+const CheckIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="10" cy="10" r="10" fill="#048724" />
+    <path
+      d="M6 10L9 13L14 7"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default CheckIcon;
