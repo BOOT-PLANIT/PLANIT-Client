db69e19815c6e731a4b34ae5b0252bd0d7899dc5
leehoegyu <leehoegyu@gmail.com>
2025-12-18 16:01:16 +0900

    fix: checkAuth 사용 제거

--- src/app/(sub)/layout.tsx
+++ src/app/(sub)/layout.tsx
@@ -1,16 +1,11 @@
-import { checkAuth } from "@/feature/auth/server";
-
+"use client";
 import styles from "./layout.module.scss";
-
-export default async function SubLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  await checkAuth();
+const SubLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={styles.layout}>
       <main className={styles.main}>{children}</main>
     </div>
   );
-}
+};
+
+export default SubLayout;
